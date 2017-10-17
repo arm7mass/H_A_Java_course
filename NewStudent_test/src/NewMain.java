dae226d7f80e2fdf7f4921c23b9cbca7286dc207
arm7mass <arm7mass@gmail.com>
2017-10-17 09:53:06 +0300

    add new main class

---
+++ NewStudent_test/src/NewMain.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ARM-PC
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.StudentNumber = 1234;
+        s1.StudentName = "abdul";
+        s1.StudentCode = "aaa111";
+        s1.BranchName = "biomed";
+        System.out.println(s1.getAllDataInString());
+        System.out.println(s1.getAllData()[0]);
+        ////////////////////////////
+        Student s2 = new Student();
+        s2.StudentNumber = 4567;
+        s2.StudentName = "salem";
+        s2.StudentCode = "bbb2222";
+        s2.BranchName = "Doctor";
+        System.out.println(s2.getAllDataInString());
+        System.out.println(s2.getAllData()[0]);
+
+    }
+
+}
