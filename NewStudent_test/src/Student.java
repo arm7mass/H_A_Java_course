b98491ffb94e28ca2ccd66c230ab4c7d691ee4c8
arm7mass <arm7mass@gmail.com>
2017-10-18 11:04:21 +0300

    constructor with initialized values

--- NewStudent_test/src/Student.java
+++ NewStudent_test/src/Student.java
@@ -1,20 +1,29 @@
 
+// the class
 public class Student {
-
+// attributes 
     public int StudentNumber;
     public String StudentName;
     public String StudentCode;
     public String BranchName;
-    private double grade ;
-    public Student (){
+    private double grade;
+    
+// constructor   
+    public Student() {
         System.out.println("\n new student is created :\n ");
-    } 
-
+        StudentNumber = 0;
+        StudentName = "empty name";
+        StudentCode = "empty code";
+        BranchName = "empty branch";
+    }
+    
+// method 1
     public String[] getAllData() {
         String strAll[] = {String.valueOf(StudentNumber), StudentName, StudentCode, BranchName};
         return strAll;
     }
-
+    
+// method 2
     public String getAllDataInString() {
         String strAll = String.valueOf(StudentNumber) + ";" + StudentName + ";" + StudentCode + ";" + BranchName;
         return strAll;
