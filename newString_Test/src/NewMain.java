ef64e3d50c2947552fd1ba51a955dbcab9f56e91
arm7mass <arm7mass@gmail.com>
2017-10-22 19:23:30 +0300

    use object to print string by string in array

--- newString_Test/src/NewMain.java
+++ newString_Test/src/NewMain.java
@@ -24,6 +24,41 @@ public class NewMain {
         c1.PrintVharByChar();
         System.out.println("//////inreverse//////");
         c1.printcharBycharinverse();
+
+        //// Usage of String Array object/////////////
+        ////option1
+        String arr[] = new String[3];
+        arr[0] = "abdul";
+        arr[1] = "nada";
+        arr[2] = "salem";
+        StringTool c2 = new StringTool(arr);
+        c2.printString();
+
+        //////option2 
+        String arr1[] = {"abdul", "salem", "Nada"};
+        StringTool c3 = new StringTool(arr1);
+        c3.printString();
+        
+        //////option3
+        String[] arr2 = {"abdul1","salem1","nada1"},
+                 arr3 = {"abdul2","salem2","nada2"},
+                 arr4 = {"abdul3","salem3","nada3"};
+        StringTool c4 = new StringTool(arr2);
+        StringTool c5 = new StringTool(arr3);
+        StringTool c6 = new StringTool(arr4);
+        c4.printString();
+        c5.printString();
+        c6.printString();
+        
+        /////// option4
+        StringTool x = new StringTool(arr2);
+        x.printString();
+        x.texts=arr3;
+        x.printString();
+        x.texts=arr4;
+        x.printString();
+        
+
     }
 
 }
