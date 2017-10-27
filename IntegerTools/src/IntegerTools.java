5aca58043427e62438b6169e6ca98fae31836302
arm7mass <arm7mass@gmail.com>
2017-10-27 15:00:24 +0300

    class to return two integer arrays elements

--- IntegerTools/src/IntegerTools.java
+++ IntegerTools/src/IntegerTools.java
@@ -5,10 +5,17 @@ public class IntegerTools {
     public int numbers[];
     private int sum1;
     private int sum2;
+    public int numbers1[];
+    public int numbers2[];
 
     // constructors 
     public IntegerTools(int numbers[]) {
         this.numbers = numbers;
+    }
+
+    public IntegerTools(int numbers1[], int numbers2[]) {
+        this.numbers1 = numbers1;
+        this.numbers2 = numbers2;
     }
 
     //methods with return  
@@ -27,4 +34,19 @@ public class IntegerTools {
         }
         System.out.println(sum2);
     }
+
+    public int[] getAdditionalTwoArray() {
+        int numadd[] = new int[numbers1.length];
+        if (numbers1.length != numbers2.length) {
+            System.out.println(" the two array is not equal ");
+            return numadd;
+        } else {
+            for (int x = 0; x > numbers1.length; x++) {
+                numadd[x] = numbers1[x] + numbers2[x];
+            }
+            return numadd;
+        }
+
+    }
+
 }
