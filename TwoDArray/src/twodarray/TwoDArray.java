bb9684edfbd25029dbf76befb80ce1cdaec7c722
arm7mass <arm7mass@gmail.com>
2017-11-05 19:27:26 +0300

    Merge Array

---
+++ TwoDArray/src/twodarray/TwoDArray.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twodarray;
+
+/**
+ *
+ * @author ARM-PC
+ */
+public class TwoDArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String namesTables [][]  = new String [2][3];
+        namesTables [0][0]="abdul1";
+        namesTables [0][1]="abdul2";
+        namesTables [0][3]="abdul3";
+        namesTables [1][0]="abdul4";        
+        namesTables [1][1]="abdul5";        
+        namesTables [1][2]="abdul6";        
+                
+    }
+    
+}
