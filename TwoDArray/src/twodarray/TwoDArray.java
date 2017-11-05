bfc9c96280577d2ae3193b6e3eefab4fbb2508a3
arm7mass <arm7mass@gmail.com>
2017-11-05 20:19:32 +0300

    for & enhanced for loop to print Two Dimensional Array

--- TwoDArray/src/twodarray/TwoDArray.java
+++ TwoDArray/src/twodarray/TwoDArray.java
@@ -16,15 +16,27 @@ public class TwoDArray {
      */
     public static void main(String[] args) {
         //two Dimensional Array
-        String namesTables [][]  = new String [2][3];
-        namesTables [0][0]="abdul1";
-        namesTables [0][1]="abdul2";
-        namesTables [0][3]="abdul3";
-        namesTables [1][0]="abdul4";        
-        namesTables [1][1]="abdul5";        
-        namesTables [1][2]="abdul6"; 
-        
-                
+        String namesTables[][] = new String[2][3];
+        namesTables[0][0] = "abdul1";
+        namesTables[0][1] = "abdul2";
+        namesTables[0][2] = "abdul3";
+        namesTables[1][0] = "abdul4";
+        namesTables[1][1] = "abdul5";
+        namesTables[1][2] = "abdul6";
+       // for loop to print  Two Dimensional Array ++++++++++
+        for (int x = 0; x < namesTables.length; x++) {
+            for (int y = 0; y < namesTables[x].length; y++) {
+                System.out.println(namesTables[x][y]);
+            }
+            System.out.println("xxxxxxxxxxxxxxxxxxxxxxx");
+        }
+        //Enhanced for loop to print  Two Dimensional Array ++++++++++
+        for (String str1[] : namesTables){
+            for (String str2 : str1){
+                System.out.print(str2+"  *  ");
+            }
+        }
+
     }
-    
+
 }
