70aa7433e0242f25dd553e0ae43704fbe043f57c
arm7mass <arm7mass@gmail.com>
2017-12-23 16:00:02 +0300

    encapsulation Example

---
+++ EncapsulationExample/src/encapsulationexample/Emp.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsulationexample;
+
+/**
+ *
+ * @author ARM-PC
+ */
+public class Emp {
+    private int _EmpNO ;
+    private String _EmpName ;
+    private String _Address ;
+
+    public int getEmpNO() {
+        return _EmpNO;
+    }
+
+    public void setEmpNO(int _EmpNO) {
+        this._EmpNO = _EmpNO;
+    }
+
+    public String getEmpName() {
+        return _EmpName;
+    }
+
+    public void setEmpName(String _EmpName) {
+        this._EmpName = _EmpName;
+    }
+
+    public String getAddress() {
+        return _Address;
+    }
+
+    public void setAddress(String _Address) {
+        this._Address = _Address;
+    }
+    
+    
+}
