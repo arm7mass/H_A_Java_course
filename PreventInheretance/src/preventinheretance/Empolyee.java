819fbd80d9dd5f6369dd5cdf019e10377d07e05a
arm7mass <arm7mass@gmail.com>
2018-01-02 10:13:18 +0300

    Prevent inheritance By using final

---
+++ PreventInheretance/src/preventinheretance/Empolyee.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package preventinheretance;
+
+/**
+ *
+ * @author ARM-PC
+ */
+public final class Empolyee {
+    public String EmployeeName;
+    public String Address ;
+    public double salary ;
+
+    public String getEmployeeName() {
+        return EmployeeName;
+    }
+
+    public void setEmployeeName(String EmployeeName) {
+        this.EmployeeName = EmployeeName;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    
+}
