622b6d5e2ea6cac5ee9731a26d77707168fe900d
arm7mass <arm7mass@gmail.com>
2018-01-04 13:04:49 +0300

    Overloading (methods with same Name and different Parameter

---
+++ overloading/src/overloading/MyName.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overloading;
+
+/**
+ *
+ * @author ARM-PC
+ */
+public class MyName {
+
+    public void PrintName() {
+        System.out.println("My Name is Abdulrhman ");
+    }
+
+    public void PrintName(String Name) {
+        System.out.println(Name);
+    }
+
+    public void PrintName(String Fname, String LName) {
+        System.out.println(Fname + " " + LName);
+    }
+
+}
