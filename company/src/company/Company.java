e73d1680d140059c0b5df382555a513637e17439
arm7mass <arm7mass@gmail.com>
2018-02-11 22:05:29 +0300

    Anonymous from interface anonymous from  class Anonymous with object

--- company/src/company/Company.java
+++ company/src/company/Company.java
@@ -17,25 +17,31 @@ public class Company {
 
         };
         e.printName();
-        
 
         // anonymouse from the class 
-        Emp a = new Emp(){
-          @Override   
-          public void printName(){
-              System.out.println("Naa1");
-              System.out.println("Nada2");
-              System.out.println("Nada3");
-          }
-          
+        Emp a = new Emp() {
+            @Override
+            public void printName() {
+                System.out.println("Naa1");
+                System.out.println("Nada2");
+                System.out.println("Nada3");
+            }
+
         };
         a.printName();
-        
-        
-        
-        
-        
-        
+        // anonymous with object without Name 
+
+        /*frm f = new frm();
+        f.setVisible(true);*/
+        //new frm().setVisible(true);
+        new Emp() {
+            @Override
+            public void printName() {
+                System.out.println("Anonymous with object with out name ");
+
+            }
+        }.printName();
+
     }
 
 }
---
+++ company/src/company/Emp.java
@@ -0,0 +1,11 @@
+package company;
+
+public class Emp implements iEmp{
+
+    @Override
+    public void printName() {
+        System.out.println("My Name: Is Abdulrhman");
+        
+    }
+
+}
