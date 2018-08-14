b9ed7f6dbd7fb575d6a4308dd8bad0263d2f0b88
arm7mass <arm7mass@gmail.com>
2018-08-14 17:52:25 +0300

    company project Project Class

--- Company_sql/src/Entity/Porject.java
+++ Company_sql/src/Entity/Porject.java
@@ -4,44 +4,44 @@ import company_sql.Tools;
 import javax.swing.JTable;
 
 public class Porject implements mainData {
-    
+
     private int ProjectNO;
     private String ProjectName;
     private String Location;
     private int DeptNO;
-    
+
     public int getProjectNO() {
         return ProjectNO;
     }
-    
+
     public void setProjectNO(int ProjectNO) {
         this.ProjectNO = ProjectNO;
     }
-    
+
     public String getProjectName() {
         return ProjectName;
     }
-    
+
     public void setProjectName(String ProjectName) {
         this.ProjectName = ProjectName;
     }
-    
+
     public String getLocation() {
         return Location;
     }
-    
+
     public void setLocation(String Location) {
         this.Location = Location;
     }
-    
+
     public int getDeptNO() {
         return DeptNO;
     }
-    
+
     public void setDeptNO(int DeptNO) {
         this.DeptNO = DeptNO;
     }
-    
+
     @Override
     public void add() {
         String strInsert = "inert into project values ("
@@ -54,45 +54,61 @@ public class Porject implements mainData {
             Tools.msgBox("Project Is Added ");
         }
     }
-    
+
     @Override
     public void update() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String strUpdate = "update project set"
+                + "ProjectName=," + ProjectName + "',"
+                + "Location ='" + Location + "',"
+                + "DeptNO=" + DeptNO
+                + "where ProjectNO=" + ProjectNO;
+        boolean isUpdate = db.go.runNonQuery(strUpdate);
+        if (isUpdate) {
+            Tools.msgBox("Project Is Updated ");
+        }
     }
-    
+
     @Override
     public void delete() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String strDelete = "delete from project"
+                + " where ProjectNO =" + ProjectNO;
+        boolean isDeleted = db.go.runNonQuery(strDelete);
+        if (isDeleted) {
+            Tools.msgBox("Project is Deleted ");
+        }
     }
-    
+
     @Override
     public String getAutoNumber() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return db.go.getAutoNumber("project", "ProjectNO");
+
     }
-    
+
     @Override
     public void getAllRows(JTable table) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        db.go.fillToJTable("project_data", table);
     }
-    
+
     @Override
     public void getOneRow(JTable table) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String strSelect = "select * from project_data"
+                + "where Project_NO=" + ProjectNO;
+        db.go.fillToJTable(strSelect, table);
     }
-    
+
     @Override
     public void getCustomRows(String statement, JTable table) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        db.go.fillToJTable(statement, table);
     }
-    
+
     @Override
     public String getValueByName(String name) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public String getNameByValue(String value) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
