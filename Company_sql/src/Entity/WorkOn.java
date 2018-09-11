9f3a23b81712953e5ce530d554b427f02b52b7e7
arm7mass <arm7mass@gmail.com>
2018-09-11 19:58:45 +0300

    company Work ON class

---
+++ Company_sql/src/Entity/WorkOn.java
@@ -0,0 +1,72 @@
+
+package Entity;
+
+import javax.swing.JTable;
+
+
+public class WorkOn implements mainData {
+    private int EmpNO ;
+    private int ProjectNO;
+
+    public int getEmpNO() {
+        return EmpNO;
+    }
+
+    public void setEmpNO(int EmpNO) {
+        this.EmpNO = EmpNO;
+    }
+
+    public int getProjectNO() {
+        return ProjectNO;
+    }
+
+    public void setProjectNO(int ProjectNO) {
+        this.ProjectNO = ProjectNO;
+    }
+
+    @Override
+    public void add() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getAutoNumber() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getAllRows(JTable table) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getOneRow(JTable table) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getCustomRows(String statement, JTable table) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getValueByName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNameByValue(String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
