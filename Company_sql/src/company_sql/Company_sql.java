44f0299face5c5d713d1331ffa3176c45693ccb1
arm7mass <arm7mass@gmail.com>
2018-09-18 19:37:09 +0300

    company Work ON form

--- Company_sql/src/company_sql/Company_sql.java
+++ Company_sql/src/company_sql/Company_sql.java
@@ -6,16 +6,18 @@ import Forms.frmEmployee;
 import Forms.frmLogin;
 import Forms.frmProject;
 import Forms.frmTest;
+import Forms.frmWorkON;
 
 public class Company_sql {
-
+    
     public static void main(String[] args) {
         //Tools.openForm(new frmLogin());
-        Tools.openForm(new frmTest());
+        //Tools.openForm(new frmTest());
         //Tools.openForm(new frmDepartment());
         //Tools.openForm(new frmEmployee());
         //Tools.openForm(new frmProject());
+        Tools.openForm(new frmWorkON());
         
     }
-
+    
 }
